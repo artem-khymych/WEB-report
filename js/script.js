eb0f3f1ff35b62d2b8def7120de0c2e8f2800415
artem-khymych <90198601+artem-khymych@users.noreply.github.com>
2024-03-12 20:50:07 +0200

    added lab 2

--- js/script.js
+++ js/script.js
@@ -1,82 +1,45 @@
-document.getElementById('block1').addEventListener('click', function(event) {
-    event.preventDefault();
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('main-content').innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', 'content/report-1/task1.html', true);
-    xhr.send();
+let lab_num = 'report-1';
+
+document.addEventListener('click', function(event) {
+    if (event.target.id.slice(0,-1) === 'block') {
+        event.preventDefault();
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                document.getElementById('main-content').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.open('GET', `content/${lab_num}/task${event.target.id[5]}.html`, true);
+        xhr.send();
+    }
 });
 
-document.getElementById('block2').addEventListener('click', function(event) {
-    event.preventDefault();
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('main-content').innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', 'content/report-1/task2.html', true);
-    xhr.send();
+document.addEventListener('click', function(event) {
+    if (event.target.id === 'button-lab-1') {
+        lab_num ='report-1';
+        console.log(event.target.id)
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                document.getElementById('sidebar-main').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.open('GET', `content/${lab_num}/page.html`, true);
+        xhr.send();
+    }
 });
-
-document.getElementById('block3').addEventListener('click', function(event) {
-    event.preventDefault();
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('main-content').innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', 'content/report-1/task3.html', true);
-    xhr.send();
-});
-
-document.getElementById('block4').addEventListener('click', function(event) {
-    event.preventDefault();
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('main-content').innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', 'content/report-1/task4.html', true);
-    xhr.send();
-});
-
-document.getElementById('block5').addEventListener('click', function(event) {
-    event.preventDefault();
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('main-content').innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', 'content/report-1/task5.html', true);
-    xhr.send();
-});
-document.getElementById('block6').addEventListener('click', function(event) {
-    event.preventDefault();
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('main-content').innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', 'content/report-1/task6.html', true);
-    xhr.send();
-});
-
-document.getElementById('block7').addEventListener('click', function(event) {
-    event.preventDefault();
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('main-content').innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open('GET', 'content/report-1/task7.html', true);
-    xhr.send();
+// ЛАБА 2
+document.addEventListener('click', function(event) {
+    if (event.target.id.slice(0,-1) === 'button-lab-') {
+        lab_num=`report-${event.target.id.slice(-1)}`;
+        event.preventDefault();
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                document.getElementById('sidebar-main').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.open('GET', `content/${lab_num}/page.html`, true);
+        xhr.send();
+    }
 });
